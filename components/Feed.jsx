90019feedb2a4547632131b53041c88ee654e501
syihabudin081 <syihabupnyk@gmail.com>
2023-09-10 20:13:50 +0700

    refactor feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,13 +2,17 @@
 
 import { useState, useEffect } from "react";
 
-import Card from "@components/Card";
+import Card from "./Card";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className="mt-16 prompt_layout">
+    <div className='mt-16 prompt_layout'>
       {data.map((post) => (
-        <Card key={post._id} post={post} handleTagClick={handleTagClick} />
+        <Card
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
       ))}
     </div>
   );
@@ -25,7 +29,7 @@ const Feed = () => {
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt");
     const data = await response.json();
-    console.log(data);
+
     setAllPosts(data);
   };
 
@@ -47,7 +51,7 @@ const Feed = () => {
     clearTimeout(searchTimeout);
     setSearchText(e.target.value);
 
-    /// debounce method
+    // debounce method
     setSearchTimeout(
       setTimeout(() => {
         const searchResult = filterPrompts(e.target.value);
@@ -64,15 +68,15 @@ const Feed = () => {
   };
 
   return (
-    <section className="feed">
-      <form className="relative w-full flex-center">
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
         <input
-          type="text"
-          placeholder="Search for a tag or a username"
+          type='text'
+          placeholder='Search for a tag or a username'
           value={searchText}
           onChange={handleSearchChange}
           required
-          className="search_input peer"
+          className='search_input peer'
         />
       </form>
 
