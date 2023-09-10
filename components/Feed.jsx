1f9dcebd321831a815ce8908ae3bce1847105c54
syihabudin081 <syihabupnyk@gmail.com>
2023-09-10 19:55:19 +0700

    fix typo

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,13 +6,9 @@ import Card from "@components/Card";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className='mt-16 prompt_layout'>
+    <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <Card
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
+        <Card key={post._id} post={post} handleTagClick={handleTagClick} />
       ))}
     </div>
   );
@@ -29,7 +25,7 @@ const Feed = () => {
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt");
     const data = await response.json();
-    console.log(data)
+    console.log(data);
     setAllPosts(data);
   };
 
@@ -51,7 +47,7 @@ const Feed = () => {
     clearTimeout(searchTimeout);
     setSearchText(e.target.value);
 
-    // debounce method
+    /// debounce method
     setSearchTimeout(
       setTimeout(() => {
         const searchResult = filterPrompts(e.target.value);
@@ -68,15 +64,15 @@ const Feed = () => {
   };
 
   return (
-    <section className='feed'>
-      <form className='relative w-full flex-center'>
+    <section className="feed">
+      <form className="relative w-full flex-center">
         <input
-          type='text'
-          placeholder='Search for a tag or a username'
+          type="text"
+          placeholder="Search for a tag or a username"
           value={searchText}
           onChange={handleSearchChange}
           required
-          className='search_input peer'
+          className="search_input peer"
         />
       </form>
 
